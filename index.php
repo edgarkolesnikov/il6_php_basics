45a19fb359776c0a242f67569ed9bb7d6a9b4541
edgarkolesnikov <edgarkolesnikow@gmail.com>
2022-01-03 22:24:43 +0200

    added extra lines

--- index.php
+++ index.php
@@ -72,6 +72,17 @@ $z = $x % $y;
 echo $z;
 echo "<br>";
 ?>
-<input type="submit">
+<form action="" method="POST">
+    <input type="submit" value="0" name="mybutton">
+    <input type="submit" value="1" name="mybutton">
+    <input type="submit" value="2" name="mybutton">
+</form>
+
+<?php
+if (isset($_POST["mybutton"]))
+{
+    echo $_POST["mybutton"];
+}
+?>
 
 
